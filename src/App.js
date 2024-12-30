9373fe11f91b818f6d86b9170b75b4a5ac3942bd
Oyematth01 <oyematth@gmail.com>
2024-12-30 09:06:57 +0100

    images added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 
 const products = [
-  { id: 1, title: "Modern Chair", price: 180, image: "https://via.placeholder.com/200" },
-  { id: 2, title: "Minimalistic Plant Pot", price: 180, image: "https://via.placeholder.com/200" },
-  { id: 3, title: "Night Stand", price: 180, image: "https://via.placeholder.com/200" },
-  { id: 4, title: "Plant Pot", price: 18, image: "https://via.placeholder.com/200" },
-  { id: 5, title: "Small Table", price: 320, image: "https://via.placeholder.com/200" },
-  { id: 6, title: "Modern Rocking Chair", price: 318, image: "https://via.placeholder.com/200" },
-  { id: 7, title: "Metallic Chair", price: 318, image: "https://via.placeholder.com/200" },
-  { id: 8, title: "Home Deco", price: 318, image: "https://via.placeholder.com/200" },
+  { id: 1, title: "Modern Chair", price: 180, image: `${process.env.PUBLIC_URL}/resources/1.jpg.webp` },
+  { id: 2, title: "Minimalistic Plant Pot", price: 180, image: `${process.env.PUBLIC_URL}/resources/2.jpg.webp` },
+  { id: 3, title: "Night Stand", price: 180, image: `${process.env.PUBLIC_URL}/resources/4.jpg.webp` },
+  { id: 4, title: "Plant Pot", price: 18, image: `${process.env.PUBLIC_URL}/resources/5.jpg.webp` },
+  { id: 5, title: "Small Table", price: 320, image: `${process.env.PUBLIC_URL}/resources/6.jpg.webp` },
+  { id: 6, title: "Modern Rocking Chair", price: 318, image: `${process.env.PUBLIC_URL}/resources/8.jpg.webp` },
+  { id: 7, title: "Metallic Chair", price: 318, image: `${process.env.PUBLIC_URL}/resources/7.jpg.webp` },
+  { id: 8, title: "Home Deco", price: 318, image: `${process.env.PUBLIC_URL}/resources/9.jpg.webp` },
+  { id: 9, title: "Modern Chair", price: 180, image: `${process.env.PUBLIC_URL}/resources/3.jpg.webp` },
 ];
 
 export default function App() {
@@ -45,14 +46,19 @@ function ProductListing() {
     <div>
       <main className="content">
           {products.map((product) => (
-            <div className="card" key={product.id}>
-              <img src={product.image} alt={product.title} />
-              <h3>{product.title}</h3>
-              <p>From ${product.price}</p>
-            </div>
+            <ProductCard key={product.id} product={product} />
           ))}
         </main>
     </div>
   );
 };
 
+function ProductCard({ product }) {
+  return (
+    <div className="card" key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>From ${product.price}</p>
+            </div>
+  )
+}
